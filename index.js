86f3b7af4b01ace00a7b05e164b8cb9b83f5572e
Dmitry Chernyatiev <d.chernyatiev@gmail.com>
2013-12-30 12:43:47 +0400

    final

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+var rows = 100;
+var colls = 100;
+
 $(document).ready(function(){
   console.log('dfd');
   for (var i = 0; i < 10; i++) {
@@ -10,12 +13,14 @@ $(document).ready(function(){
   }
   init();
   draw(LifeField);
+  LifeFiled = process_state();
+  draw(LifeField);
 });
 
 var LifeField = new Array();
 
 function init() {
-  init_array();
+  init_array(LifeField);
   LifeField[0][1] = 1;
   LifeField[1][2] = 1;
   LifeField[2][0] = 1;
@@ -23,13 +28,52 @@ function init() {
   LifeField[2][2] = 1;
 }
 
-function init_array(){
-  for (var i = 0; i < 10; i++) {
-    LifeField[i] = new Array();
+function init_array(arr){
+  for (var i = 0; i < rows; i++) {
+    arr[i] = new Array(cols);
   };
 }
 
-function draw(array){
+function draw(ar){
+  for (var i = 0; i < ar.length; ++i) {
+    for (var j = 0; j < ar[i].length; ++j) {
+      if (ar[i][j] == 1) {
+        console.log($("#" + i + "-" + j));
+        $("#" + i + "-" + j).css('background-color', 'black');
+      } else {
+        $("#" + i + "-" + j).css('background-color', 'white');
+      }
+    }
+  }
+}
 
+function process_state() {
+  var res_arr = new Array();
+  init_array(res_arr);
+
+  for (var i = 0; i < res_arr.length; ++i){
+    for (var j = 0; j < res_arr[i].length; ++j) {
+      res_arr[i][j] = check(i, j);
+      console.log(i, j, res_arr[i][j]);
+    }
+  }
+  return res_arr;
+}
+
+function check(i, j) {
+  var life_count = 0;
+  for (var k = i - 1; k <= i + 1; ++k) {
+    for (var h = j - 1; h <= j + 1; ++h) {
+      if (((k >= 0) && (k < rows)) &&
+          ((h >= 0) && (h < cols)) &&
+            ((k != i) && (h != j))) {
+        if (LifeField[k][h] == 1)
+          life_count++;
+      }
+    }
+  }
+  if ((LifeField[i][j] == 0) && (life_count == 3)) return 1;
+  if ((LifeFiled[i][j] == 1) && ((life_count == 2) || (life_count == 3))) return 1;
+  return 0;
 }
 
