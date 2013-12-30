a1ca75c0a4ce58c6d29fec7fc63fa9a3ad9addb2
plugin73 <laplugin73@gmail.com>
2013-12-30 12:54:28 +0100

    canvas

--- index.js
+++ index.js
@@ -1,22 +1,54 @@
 $(document).ready(function(){
   console.log('dfd');
-  for (var i = 0; i < 10; i++) {
-    var row = "<tr height='10'>";
-    for (var j = 0; j < 10; j++) {
-      row += "<td id='" + i + "-" + j + "' width='5' ></td>";
-    }
-    row += '</tr>';
-    $('#life-field').append(row);
-  }
-  init();
-  draw(LifeField);
+  LifeField[4][4] = 1;
+  draw();
 });
 
 var LifeField = new Array();
 
+for (var i = 0; i < 10; i++) {
+  LifeField[i] = [];
+  for (var j = 0; j < 10; j++) {
+    LifeField[i][j] = 0;
+  }
+}
+
+var canvas = document.getElementById("example");
+canvas.addEventListener("mousedown", getPosition, false);
+
+function getPosition(event)
+{
+  var x = event.x;
+  var y = event.y;
+
+  var canvas = document.getElementById("example");
+
+  x -= canvas.offsetLeft;
+  y -= canvas.offsetTop;
+
+  LifeField[Math.floor(y / 10) ][ Math.floor(x / 10)] = 1;
+  draw(LifeField);
+}
 
 function draw(array){
 
+  var example = document.getElementById("example"),
+  ctx     = example.getContext('2d');
+  x =0;
+  y= 0;
+  for (var i = 0; i < 10; i++) {
+    x = 0;
+    for (var j = 0; j < 10; j++) {
+      if (LifeField[i][j] == 1) {
+        ctx.fillStyle = "green";
+      }else{
+        ctx.fillStyle = "gray";
+      }
+      ctx.fillRect(x, y, 10, 10);
+      x+=10;
+    }
+    y += 10;
+  }
 }
 
 check_array = function(arr) {
